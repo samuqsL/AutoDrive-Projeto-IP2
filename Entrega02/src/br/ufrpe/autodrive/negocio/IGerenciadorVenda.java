4533d13a2cde0375eed449a9c30f776460560e5d
samuqsL <samuelcely123@gmail.com>
2026-05-09 14:32:38 -0300

    Update IGerenciadorVenda.java

--- Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorVenda.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorVenda.java
@@ -1,5 +1,18 @@
 package br.ufrpe.autodrive.negocio;
 
+import br.ufrpe.autodrive.negocio.beans.Cliente;
+import br.ufrpe.autodrive.negocio.beans.Vendedor;
+import br.ufrpe.autodrive.negocio.beans.Veiculo;
+
 public interface IGerenciadorVenda {
 
+    /**
+     * Tenta realizar uma venda.
+     * @return true se passar em todas as regras de negócio e for salva, false caso contrário.
+     */
+    boolean efetuarVenda(Cliente c, Vendedor v, Veiculo veic, double entrada);
+
+    // Você também pode adicionar outros métodos que a TelaVenda vai precisar
+    // void cancelarVenda(String id);
+    // Venda procurarVenda(String cpfCliente);
 }
