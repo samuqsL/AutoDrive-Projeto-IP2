51ac6f635f76eb3d7c2ab7ba0a88dfe5d20eeff7
yuricabral-kkk <yuri.cabral@ufrpe.br>
2026-05-10 17:56:43 -0300

    Update IRepositorioTD.java

--- Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioTD.java
+++ Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioTD.java
@@ -4,9 +4,6 @@ import br.ufrpe.autodrive.negocio.beans.TestDrive;
 import java.util.List;
 
 public interface IRepositorioTD {
-
     void adicionarTestDrive(TestDrive td);
-
     List<TestDrive> listarTestDrives();
-
 }
