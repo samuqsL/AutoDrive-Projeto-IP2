5d8f9067cd87dd0fbfa97b0294d5300f82471c8b
samuqsL <samuelcely123@gmail.com>
2026-05-10 20:30:12 -0300

    Update IGerenciadorTestDrive.java

--- Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorTestDrive.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorTestDrive.java
@@ -1,8 +1,5 @@
 package br.ufrpe.autodrive.negocio;
 
-import br.ufrpe.autodrive.negocio.beans.Cliente;
-import br.ufrpe.autodrive.negocio.beans.Veiculo;
-
 public interface IGerenciadorTestDrive {
     boolean agendarTestDrive(String cpf, String chassi);
 }
