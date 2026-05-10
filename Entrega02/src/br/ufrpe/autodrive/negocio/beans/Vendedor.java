3e927f3c87d1efee7fd1589fd1b91ddf49405551
samuqsL <samuelcely123@gmail.com>
2026-05-09 22:06:07 -0300

    Refactor Vendedor class to extend Pessoa

--- Entrega02/src/br/ufrpe/autodrive/negocio/beans/Vendedor.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/beans/Vendedor.java
@@ -1,40 +1,33 @@
 package br.ufrpe.autodrive.negocio.beans;
 
-public class Vendedor {
+public class Vendedor extends Pessoa {
+    private double comissao;
+    private double percentualComissao;
 
-  private String nome;
-  private double comissao;
-  private double percentualComissao;
-
-  //construtor default (sem valores - precisa settar atributos posteriormente!)
-  public Vendedor() {}
-  
-  //construtor principal da classe!
-  public Vendedor(String nome, double percentualComissao){
-    this.nome = nome;
-    this.percentualComissao = percentualComissao;
-    this.comissao = 0;
-  }
-
-  //getters (pegar valor)
-  public String getNome() {return this.nome;}
-  public double getComissao() {return this.comissao;}
-  public double getPercentualComissao() {return this.percentualComissao;}
-  //setters
-  public void setNome(String nome) {this.nome = nome;}
-  
-  public void setComissao(double comissao) {
-    if (comissao < 0) {
-      throw new IllegalArgumentException("Comissão inválida"); //Impede que a comissão seja alterada para negativo!
+    // Construtor Default (opcional, mas bom ter)
+    public Vendedor() {
+        super();
     }
-    this.comissao = comissao;  
-  }
-  public void setPercentualComissao(double percentualComissao){
-    if (percentualComissao < 0) {
-      throw new IllegalArgumentException("Percentual de comissao Inválido");
+  
+    public Vendedor(String nome, double percentualComissao){
+        // Repassa o nome para a classe Pessoa
+        super(nome, null, null); 
+        this.percentualComissao = percentualComissao;
+        this.comissao = 0;
     }
-    this.percentualComissao = percentualComissao;
-  }
 
-  //Metodos (...)
+    // Getters e Setters específicos do Vendedor
+    public double getComissao() { return comissao; }
+    
+    public void setComissao(double comissao) {
+        if (comissao < 0) throw new IllegalArgumentException("Comissão inválida");
+        this.comissao = comissao;  
+    }
+
+    public double getPercentualComissao() { return percentualComissao; }
+
+    public void setPercentualComissao(double percentualComissao){
+        if (percentualComissao < 0) throw new IllegalArgumentException("Percentual inválido");
+        this.percentualComissao = percentualComissao;
+    }
 }
