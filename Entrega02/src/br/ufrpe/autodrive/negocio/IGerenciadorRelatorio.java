c0c26527b58e5acb011c9d187bb6beeeff5c96c1
OTAVIO-SANTIAGO <santiago888f1@gmail.com>
2026-05-10 22:44:18 -0300

    Update IGerenciadorRelatorio.java

--- Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorRelatorio.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorRelatorio.java
@@ -1,5 +1,10 @@
 package br.ufrpe.autodrive.negocio;
 
-public interface IGerenciadorRelatorio {
+import java.util.List;
+import br.ufrpe.autodrive.negocio.beans.Venda;
+import br.ufrpe.autodrive.negocio.beans.OrdemServico;
 
+public interface IGerenciadorRelatorio {
+    List<Venda> gerarRelatorioVendas();
+    List<OrdemServico> gerarRelatorioOS();
 }
