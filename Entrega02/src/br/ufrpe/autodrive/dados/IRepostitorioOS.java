e848d9c77e454b6f069b16cca176832ae2d403f2
OTAVIO-SANTIAGO <santiago888f1@gmail.com>
2026-05-13 20:03:13 -0300

    Update IRepostitorioOS.java

--- Entrega02/src/br/ufrpe/autodrive/dados/IRepostitorioOS.java
+++ Entrega02/src/br/ufrpe/autodrive/dados/IRepostitorioOS.java
@@ -1,5 +1,15 @@
 package br.ufrpe.autodrive.dados;
 
-public interface IRepostitorioOS {
+import java.util.List;
+import br.ufrpe.autodrive.negocio.beans.OrdemServico;
 
+public interface IRepositorioOS {
+    
+    void salvar(OrdemServico os);
+    
+    List<OrdemServico> listarTodas();
+    
+    OrdemServico buscarPorNumero(int numero);
+    
+    void remover(int numero);
 }
