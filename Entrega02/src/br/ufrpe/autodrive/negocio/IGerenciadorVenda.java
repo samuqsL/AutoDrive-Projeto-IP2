c40ed11ede2416864968c12f9e66fc7d7bb5311a
samuqsL <samuelcely123@gmail.com>
2026-05-13 23:45:28 -0300

    Update IGerenciadorVenda interface for venda method

--- Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorVenda.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorVenda.java
@@ -1,21 +1,13 @@
 package br.ufrpe.autodrive.negocio;
 
-// IMPORTS NECESSÁRIOS para reconhecer as Beans e as Listas
-import br.ufrpe.autodrive.negocio.beans.Venda;
 import br.ufrpe.autodrive.negocio.beans.Notificacao;
+import br.ufrpe.autodrive.negocio.beans.Venda;
 import java.util.List;
 
 public interface IGerenciadorVenda {
-
-    // 1. Recebe String e double da Tela e retorna se deu certo
-    boolean efetuarVenda(String cpfCliente, double entrada);
-
-    // 2. Retorna a lista de notificações para a Tela
+    // Agora recebe o nome do vendedor para buscá-lo no repositório
+    boolean efetuarVenda(String cpfCliente, String nomeVendedor, double entrada);
     List<Notificacao> listarAlertasRevisao();
-
-    // 3. Retorna o objeto Venda completo (precisa do import da bean Venda)
     Venda procurarVenda(String cpf);
-
-    // 4. Método de remoção simples
     void removerVenda();
 }
