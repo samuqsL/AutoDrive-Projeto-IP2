5e1ee15971c204aa1f7e61b60fbbf07208152acb
samuqsL <samuelcely123@gmail.com>
2026-05-14 14:25:16 -0300

    Fix imports and improve comments in GerenciadorRelatorio
    Correct import statements and update comments for clarity.

--- Entrega02/src/br/ufrpe/autodrive/negocio/GerenciadorRelatorio.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/GerenciadorRelatorio.java
@@ -1,6 +1,8 @@
 package br.ufrpe.autodrive.negocio;
 
-import br.ufrpe.autodrive.negocio.beans.Relatorio;
+import br.ufrpe.autodrive.negocio.Relatorio; // Import correto
+import br.ufrpe.autodrive.negocio.beans.Venda;
+import br.ufrpe.autodrive.negocio.beans.OrdemServico;
 import br.ufrpe.autodrive.dados.IRepositorioVendas;
 import br.ufrpe.autodrive.dados.IRepositorioOS;
 
@@ -8,7 +10,7 @@ public class GerenciadorRelatorio implements IGerenciadorRelatorio {
     private IRepositorioVendas repoVendas;
     private IRepositorioOS repoOS;
 
-    // O construtor recebe as interfaces dos repositórios (Injeção de Dependência)
+    // O construtor recebe os repositórios (Injeção de Dependência)
     public GerenciadorRelatorio(IRepositorioVendas repoVendas, IRepositorioOS repoOS) {
         this.repoVendas = repoVendas;
         this.repoOS = repoOS;
@@ -16,8 +18,7 @@ public class GerenciadorRelatorio implements IGerenciadorRelatorio {
 
     @Override
     public Relatorio gerarDadosRelatorio() {
-        // Buscamos as listas completas (que já retornam cópias seguras nos repositórios)
-        // e instanciamos o objeto Relatorio que contém as regras de filtro e cálculo.
+        // Pega os dados brutos e cria o objeto Relatorio que tem as lógicas de filtro
         return new Relatorio(
             repoVendas.listarTodasVendas(), 
             repoOS.listarTodas()
