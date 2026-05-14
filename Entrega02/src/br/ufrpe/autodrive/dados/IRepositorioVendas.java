afcca26ebe84cdb44f23f9739e99242dbca73514
samuqsL <samuelcely123@gmail.com>
2026-05-14 11:16:15 -0300

    Update removerVenda method to accept an argument

--- Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioVendas.java
+++ Entrega02/src/br/ufrpe/autodrive/dados/IRepositorioVendas.java
@@ -6,6 +6,6 @@ import java.util.List;
 public interface IRepositorioVendas {
     void adicionarVenda(Venda venda);
     Venda procurarVenda(String cpf);
-    void removerVenda();
+    void removerVenda(int numero);
     List<Venda> listarTodasVendas();
 }
