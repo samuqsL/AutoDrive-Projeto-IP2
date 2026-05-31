192ae1c41ec2e3e5bab2f5f10db67642f73caa5d
samuqsL <samuelcely123@gmail.com>
2026-05-31 15:53:43 -0300

    Add overloaded method for sale with date parameter

--- Entrega03/src/br/ufrpe/autodrive/negocio/IGerenciadorVenda.java
+++ Entrega03/src/br/ufrpe/autodrive/negocio/IGerenciadorVenda.java
@@ -3,13 +3,14 @@ package br.ufrpe.autodrive.negocio;
 import java.util.List;
 import br.ufrpe.autodrive.negocio.beans.Venda;
 import br.ufrpe.autodrive.negocio.beans.Notificacao;
+import java.time.LocalDateTime;
 
 public interface IGerenciadorVenda {
 
     boolean efetuarVenda(int numero, String cpfCliente, String chassi, String nomeVendedor, double entrada);
     
     // 🟢 NOVA SOBRECARGA PARA RECEBER A DATA DA TELA
-    boolean efetuarVenda(int numero, String cpfCliente, String chassi, String nomeVendedor, double entrada, java.time.LocalDateTime dataDigitada);
+    boolean efetuarVenda(int numero, String cpfCliente, String chassi, String nomeVendedor, double entrada, LocalDateTime dataDigitada);
 
     List<Notificacao> listarAlertasRevisao();
     Venda procurarVenda(int numero);
