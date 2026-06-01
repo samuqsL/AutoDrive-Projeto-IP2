9392158baee1e9cdf0fb8634448bd65b496fd6de
samuqsL <samuelcely123@gmail.com>
2026-05-31 21:04:40 -0300

    Update IGerenciadorTestDrive.java

--- Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorTestDrive.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/IGerenciadorTestDrive.java
@@ -1,5 +1,10 @@
 package br.ufrpe.autodrive.negocio;
 
+import java.time.LocalDateTime;
+
 public interface IGerenciadorTestDrive {
     boolean agendarTestDrive(String cpf, String chassi);
+    
+    // 🟢 Nova sobrecarga que aceita a data vinda da interface gráfica
+    boolean agendarTestDrive(String cpf, String chassi, LocalDateTime dataDigitada);
 }
