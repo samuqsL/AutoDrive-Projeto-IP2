e8bbdbe6870cee3bc893b6e7463b5319988565c1
samuqsL <samuelcely123@gmail.com>
2026-05-31 21:03:44 -0300

    Update TestDrive.java

--- Entrega02/src/br/ufrpe/autodrive/negocio/beans/TestDrive.java
+++ Entrega02/src/br/ufrpe/autodrive/negocio/beans/TestDrive.java
@@ -1,15 +1,28 @@
 package br.ufrpe.autodrive.negocio.beans;
 
+import java.time.LocalDateTime;
+
 public class TestDrive {
 
     private Cliente cliente;
     private Veiculo veiculo;
+    private LocalDateTime dataTestDrive; // 🟢 Novo Atributo
 
-    public TestDrive() {}
+    public TestDrive() {
+        this.dataTestDrive = LocalDateTime.now(); // 🟢 Garante uma data padrão se nascer vazio
+    }
     
     public TestDrive(Cliente cliente, Veiculo veiculo) {
         this.cliente = cliente;
         this.veiculo = veiculo;
+        this.dataTestDrive = LocalDateTime.now(); // 🟢 Evita efeito cascata em chamadas antigas
+    }
+
+    // 🟢 Novo construtor para quando a tela ou a Main quiserem passar uma data específica
+    public TestDrive(Cliente cliente, Veiculo veiculo, LocalDateTime dataTestDrive) {
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+        this.dataTestDrive = dataTestDrive;
     }
 
     public boolean agendar() {
@@ -29,4 +42,8 @@ public class TestDrive {
     public void setCliente(Cliente cliente) { this.cliente = cliente; }
     public Veiculo getVeiculo() { return veiculo; }
     public void setVeiculo(Veiculo veiculo) { this.veiculo = veiculo; }
+    
+    // 🟢 Novos Getters e Setters
+    public LocalDateTime getDataTestDrive() { return dataTestDrive; }
+    public void setDataTestDrive(LocalDateTime dataTestDrive) { this.dataTestDrive = dataTestDrive; }
 }
