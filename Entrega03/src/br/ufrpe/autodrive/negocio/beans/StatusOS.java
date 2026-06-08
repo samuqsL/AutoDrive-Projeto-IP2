4d7039a54304558d093304ae915676d3642e6600
yurineves-000 <yuri.cabral@ufrpe.br>
2026-06-08 04:56:00 -0300

    Update StatusOS.java

--- Entrega03/src/br/ufrpe/autodrive/negocio/beans/StatusOS.java
+++ Entrega03/src/br/ufrpe/autodrive/negocio/beans/StatusOS.java
@@ -1,8 +1,9 @@
 package br.ufrpe.autodrive.negocio.beans;
 
 public enum StatusOS {
-    ABERTA,               
-    PROCESSO_MANUTENCAO,  
-    PAGA,
-    FINALIZADA
+    // FUNÇÃO LOCALIZADA: Estados da OS controlados pela fila automática da oficina
+    ABERTA,               // Significa que está na fila aguardando mecânico disponível
+    PROCESSO_MANUTENCAO,  // Significa que a OS já possui um mecânico trabalhando nela
+    PAGA,                 // Mantido do fluxo original
+    FINALIZADA            // Significa que o serviço acabou e incrementará a produtividade
 }
