024f69a5ec56f7c4f34a3e1dd40516e6d55abdcb
yurineves-000 <yuri.cabral@ufrpe.br>
2026-06-08 04:56:55 -0300

    Update Mecanico.java

--- Entrega03/src/br/ufrpe/autodrive/negocio/beans/Mecanico.java
+++ Entrega03/src/br/ufrpe/autodrive/negocio/beans/Mecanico.java
@@ -1,33 +1,45 @@
 package br.ufrpe.autodrive.negocio.beans;
 
+/**
+ * Classe Mecanico - Herdando de Pessoa com controle de produtividade individual
+ */
 public class Mecanico extends Pessoa {
-    
-    private static final long serialVersionUID = 1L;
-    
-    private int produtividade;
+	
+	private static final long serialVersionUID = 1L;
+	
+    // FUNÇÃO LOCALIZADA: Atributos de controle individual e disponibilidade
+    private int produtividade; // Substituiu o antigo 'bonus' por contador de OS finalizadas
     private boolean disponivel;
 
+    // Construtor Default
     public Mecanico() {
         super();
         this.produtividade = 0;
     }
 
+    // FUNÇÃO LOCALIZADA: Construtor adaptado para inicializar com nome e disponibilidade
     public Mecanico(String nome, boolean disponivel) {
         super(nome, null, null); 
+        this.produtividade = 0; // Inicia sempre com zero ordens concluídas
         this.disponivel = disponivel;
-        this.produtividade = 0;
     }
 
+    // --- Métodos Específicos do Mecânico ---
+
+    // FUNÇÃO LOCALIZADA: Getters, Setters e Incrementador de Produtividade
     public int getProdutividade() {
         return produtividade;
     }
 
     public void setProdutividade(int produtividade) {
+        if (produtividade < 0) {
+            throw new IllegalArgumentException("A produtividade não pode ser negativa.");
+        }
         this.produtividade = produtividade;
     }
 
     public void incrementarProdutividade() {
-        this.produtividade++;
+        this.produtividade++; // Acrescenta +1 sempre que concluir uma OS
     }
 
     public boolean isDisponivel() {
