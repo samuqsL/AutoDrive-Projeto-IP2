603c24b659380126e670206dbf137e81bb4f4867
yurineves-000 <yuri.cabral@ufrpe.br>
2026-06-08 01:33:42 -0300

    Update IGerenciadorOficina.java

--- Entrega03/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
+++ Entrega03/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
@@ -2,15 +2,18 @@ package br.ufrpe.autodrive.negocio;
 
 import br.ufrpe.autodrive.negocio.beans.MaoDeObra;
 import br.ufrpe.autodrive.negocio.beans.Pecas;
+import br.ufrpe.autodrive.negocio.beans.Mecanico;
+import br.ufrpe.autodrive.negocio.beans.OrdemServico;
+import java.util.List;
 
 public interface IGerenciadorOficina {
-    // Agora recebe CPF e Chassi para buscar os objetos no repositório
-    boolean abrirOS(int numero, String dataAbertura, String cpfCliente, String chassiVeiculo);
     
-    // Métodos de registro
+    int abrirOS(String cpfCliente, String chassiVeiculo);
+    
     boolean registrarPecaNaOS(int numeroOS, Pecas peca, int quantidade);
     boolean registrarServicoNaOS(int numeroOS, MaoDeObra servico);
-    
-    // Finalização
     boolean finalizarServico(int numeroOS);
+    
+    void adicionarMecanico(Mecanico m);
+    List<OrdemServico> listarHistoricoOSFinalizadas();
 }
