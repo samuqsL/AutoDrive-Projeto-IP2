0d91008d35c17660fad72cf01cd0ea733139ce8a
yurineves-000 <yuri.cabral@ufrpe.br>
2026-06-08 05:00:12 -0300

    Update IGerenciadorOficina.java

--- Entrega03/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
+++ Entrega03/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
@@ -2,18 +2,14 @@ package br.ufrpe.autodrive.negocio;
 
 import br.ufrpe.autodrive.negocio.beans.MaoDeObra;
 import br.ufrpe.autodrive.negocio.beans.Pecas;
-import br.ufrpe.autodrive.negocio.beans.Mecanico;
-import br.ufrpe.autodrive.negocio.beans.OrdemServico;
-import java.util.List;
 
 public interface IGerenciadorOficina {
     
-    int abrirOS(String cpfCliente, String chassiVeiculo);
+    // FUNÇÃO LOCALIZADA: Assinatura modificada para suportar criação automatizada
+    boolean abrirOS(String cpfCliente, String chassiVeiculo);
     
     boolean registrarPecaNaOS(int numeroOS, Pecas peca, int quantidade);
     boolean registrarServicoNaOS(int numeroOS, MaoDeObra servico);
-    boolean finalizarServico(int numeroOS);
     
-    void adicionarMecanico(Mecanico m);
-    List<OrdemServico> listarHistoricoOSFinalizadas();
+    boolean finalizarServico(int numeroOS);
 }
