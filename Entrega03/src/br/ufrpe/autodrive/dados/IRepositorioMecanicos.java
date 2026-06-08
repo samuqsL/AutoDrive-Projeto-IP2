0eca587933093dc361b93de90ef38b1175f0dd32
yurineves-000 <yuri.cabral@ufrpe.br>
2026-06-08 15:41:37 -0300

    Update IRepositorioMecanicos.java

--- Entrega03/src/br/ufrpe/autodrive/dados/IRepositorioMecanicos.java
+++ Entrega03/src/br/ufrpe/autodrive/dados/IRepositorioMecanicos.java
@@ -4,9 +4,12 @@ import br.ufrpe.autodrive.negocio.beans.Mecanico;
 import java.util.List;
 
 public interface IRepositorioMecanicos {
-    //CRUDS padrão dos repositórios!
+    // CRUDS padrão dos repositórios
     void adicionarMecanico(Mecanico m);
-    Mecanico procurarMecanico(String nome); // Busca pelo nome, já que Mecanico não tem CPF visível na Main
+    Mecanico procurarMecanico(String nome); 
     void removerMecanico(String nome);
     List<Mecanico> listarTodos();
+    
+    // Novo método para persistência de alterações de estado dos mecânicos
+    void atualizarMecanico(Mecanico m);
 }
