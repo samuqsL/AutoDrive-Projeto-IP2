ad81ee2da3bb4a731b7509b35f5a8b973e9277d7
samuqsL <samuelcely123@gmail.com>
2026-06-14 13:40:00 -0300

    Refactor IGerenciadorTestDrive interface methods
    Updated method signature for cancelling test drives.

--- Entrega04/src/br/ufrpe/autodrive/negocio/IGerenciadorTestDrive.java
+++ Entrega04/src/br/ufrpe/autodrive/negocio/IGerenciadorTestDrive.java
@@ -9,14 +9,10 @@ import br.ufrpe.autodrive.negocio.beans.Veiculo;
 
 public interface IGerenciadorTestDrive {
     boolean agendarTestDrive(String cpf, String chassi);
-    
-    // Nova sobrecarga que aceita a data vinda da interface gráfica
     boolean agendarTestDrive(String cpf, String chassi, LocalDateTime dataDigitada);
+    List<TestDrive> listarTestDrives();
     
-    public List<TestDrive> listarTestDrives();
-    
-    // metodos novos
     List<Cliente> listarTodosClientes();
     List<Veiculo> listarTodosVeiculos();
-    boolean cancelarTestDrive(String hash);
+    boolean cancelarTestDrive(String id);
 }
