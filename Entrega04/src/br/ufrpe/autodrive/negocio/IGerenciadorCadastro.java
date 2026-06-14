7b0190498cdb5ad9328ae80ee4acc3d66b6b6a24
samuqsL <samuelcely123@gmail.com>
2026-06-14 01:30:22 -0300

    Add methods for client and vehicle registration

--- Entrega04/src/br/ufrpe/autodrive/negocio/IGerenciadorCadastro.java
+++ Entrega04/src/br/ufrpe/autodrive/negocio/IGerenciadorCadastro.java
@@ -1,5 +1,17 @@
 package br.ufrpe.autodrive.negocio;
 
+import java.util.List;
+import br.ufrpe.autodrive.negocio.beans.Cliente;
+import br.ufrpe.autodrive.negocio.beans.Veiculo;
+
 public interface IGerenciadorCadastro {
-  
+    
+    // Métodos Principais de Regra de Negócio
+    void cadastrarCliente(String nome, String cpf, String cnh, String email, String telefone) throws Exception;
+    
+    void cadastrarVeiculo(String chassi, String renavam, String modelo, int ano, double preco, boolean ehSeminovo, double kmInicial) throws Exception;
+    
+    // Métodos Auxiliares para alimentar as Tabelas da Janela
+    List<Cliente> listarClientes();
+    List<Veiculo> listarVeiculos();
 }
