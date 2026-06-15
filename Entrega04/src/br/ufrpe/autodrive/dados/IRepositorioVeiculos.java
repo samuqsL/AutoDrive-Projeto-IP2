8ba87a0c05b83a7ba29d86dfcd4035a08d5ff599
Artur Cândido <arturcdn10@gmail.com>
2026-06-15 17:50:33 -0300

    Update IRepositorioVeiculos.java
    resolvido o erro que o veiculo não era cadastrado

--- Entrega04/src/br/ufrpe/autodrive/dados/IRepositorioVeiculos.java
+++ Entrega04/src/br/ufrpe/autodrive/dados/IRepositorioVeiculos.java
@@ -4,7 +4,7 @@ import br.ufrpe.autodrive.negocio.beans.Veiculo;
 import java.util.List;
 
 public interface IRepositorioVeiculos {
-    void adicionarVeiculo(Veiculo v);
+    void adicionarVeiculo(Veiculo novoVeiculo);
     Veiculo procurarVeiculo(String chassi); // Busca por Chassi (ID único)
     List<Veiculo> listarTodos();
     void removerVeiculo(String chassi);
