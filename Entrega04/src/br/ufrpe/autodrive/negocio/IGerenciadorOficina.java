7c0d1d2edb0ba0d69cd85a82c10e2ecb47ff6ac8
yurineves-000 <yuri.cabral@ufrpe.br>
2026-06-14 22:25:19 -0300

    Update IGerenciadorOficina.java

--- Entrega04/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
+++ Entrega04/src/br/ufrpe/autodrive/negocio/IGerenciadorOficina.java
@@ -1,13 +1,14 @@
 package br.ufrpe.autodrive.negocio;
 
+import br.ufrpe.autodrive.negocio.beans.MaoDeObra;
 import br.ufrpe.autodrive.negocio.beans.Pecas;
-import java.util.List;
 
 public interface IGerenciadorOficina {
     
-    boolean abrirOS(String cpfCliente, String chassiVeiculo, String codigoPeca, int quantidadePeca, String descricaoMaoDeObra, double valorMaoDeObra) throws Exception;
+    boolean abrirOS(String cpfCliente, String chassiVeiculo, String codigoPeca, int quantidadePeca, String descricaoMaoDeObra, double valorMaoDeObra);
+    
     boolean registrarPecaNaOS(int numeroOS, Pecas peca, int quantidade);
-    boolean registrarServicoNaOS(int numeroOS, br.ufrpe.autodrive.negocio.beans.MaoDeObra servico);
+    boolean registrarServicoNaOS(int numeroOS, MaoDeObra servico);
+    
     boolean finalizarServico(int numeroOS);
-    List<Pecas> listarPecasDisponiveis();
 }
